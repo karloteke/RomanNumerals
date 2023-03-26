86fc1285a61776cbfe7e9ba0af0461e6080e791f
Carlota Cetina <carlotika26@gmail.com>
2023-03-26 17:21:25 +0100

    Commit 3
    Test failed with number 2

--- src/main/java/com/svalero/RomanNumeralsTest.java
+++ src/main/java/com/svalero/RomanNumeralsTest.java
@@ -11,5 +11,7 @@ public class RomanNumeralsTest {
     public void test_convert_to_roman() {
         String resultOne = this.romanNumerals.convertToRoman(1);
         Assert.assertTrue(resultOne.equals("I"));
+        String resultTwo = this.romanNumerals.convertToRoman(2);
+        Assert.assertTrue(resultTwo.equals("II"));
     }
 }
