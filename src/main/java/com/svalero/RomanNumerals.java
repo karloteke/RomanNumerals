ee2f7e6f6532eae89806b2f1840c104bf782801b
Carlota Cetina <carlotika26@gmail.com>
2023-03-26 17:24:14 +0100

    Commit 4
     Test passed with number 2

--- src/main/java/com/svalero/RomanNumerals.java
+++ src/main/java/com/svalero/RomanNumerals.java
@@ -2,6 +2,9 @@ package com.svalero;
 public class RomanNumerals {
 
     public static String  convertToRoman(int decimal) {
+        if(decimal == 2){
+            return "II";
+        }
         return "I";
     }
 }
--- src/main/java/com/svalero/RomanNumeralsTest.java
+++ src/main/java/com/svalero/RomanNumeralsTest.java
@@ -2,9 +2,6 @@ package com.svalero;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static sun.nio.cs.Surrogate.is;
-
 public class RomanNumeralsTest {
     RomanNumerals romanNumerals = new RomanNumerals();
     @Test
