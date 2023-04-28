52c6e958ca60b185e7206dc18f1d75a6d42fb0a0
Carlota Cetina <carlotika26@gmail.com>
2023-04-28 01:26:14 +0100

    Linter improvements2

--- src/test/java/RomanNumerals.java
+++ src/test/java/RomanNumerals.java
@@ -1,16 +1,28 @@
 public final class RomanNumerals {
     RomanNumerals() {} // constructor privado y final para evitar instanciación
 
+    /**
+     * El valor mínimo que se pueden convertir a números romanos.
+     */
     private static final int MIN_VALUE = 1;
+
+    /**
+     * El valor máximo que se pueden convertir a números romanos.
+     */
     private static final int MAX_VALUE = 3000;
 
+    /**
+     * Convierte un número entero en números romano.
+     */
     public static String convertToRoman(final int number) {
         if (number < MIN_VALUE || number > MAX_VALUE) {
             throw new IllegalArgumentException("El número debe estar entre 1 y 3000.");
         }
 
-        final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols =
+                {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        final int[] values =
+                {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         final StringBuilder roman = new StringBuilder();
         int remainingNumber = number;
@@ -21,7 +33,6 @@ public final class RomanNumerals {
                 remainingNumber -= values[i];
             }
         }
-
         return roman.toString();
     }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -2,15 +2,45 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RomanNumeralsTest {
+    /**
+     * Conversión numero 1.
+     */
     private static final int ONE = 1;
+    /**
+     * Conversión numero 2.
+     */
     private static final int TWO = 2;
+    /**
+     * Conversión numero 3.
+     */
     private static final int THREE = 3;
+    /**
+     * Conversión numero 4.
+     */
     private static final int FOUR = 4;
+    /**
+     * Conversión numero 5.
+     */
     private static final int FIVE = 5;
+    /**
+     * Conversión numero 7.
+     */
     private static final int SEVEN = 7;
+    /**
+     * Conversión numero 9.
+     */
     private static final int NINE = 9;
+    /**
+     * Conversión numero 10.
+     */
     private static final int TEN = 10;
+    /**
+     * Conversión numero 18.
+     */
     private static final int EIGHTEEN = 18;
+    /**
+     * Conversión numero 30.
+     */
     private static final int THIRTY = 30;
 
     private final RomanNumerals romanNumerals = new RomanNumerals();
